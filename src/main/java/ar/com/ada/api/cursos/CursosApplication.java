b765bbca35b25a6b8d8612d9ad0c3ccc6193442e
lumaldonado <lulimaldonado98@hotmail.com>
2021-08-25 16:40:24 -0300

    primer commit

---
+++ src/main/java/ar/com/ada/api/cursos/CursosApplication.java
@@ -0,0 +1,13 @@
+package ar.com.ada.api.cursos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursosApplication.class, args);
+	}
+
+}
